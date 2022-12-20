43950feb44b7cca6166a0acce51b1788741e68b8
Kiss Tamás <kisstkondoros@gmail.com>
2022-12-20 20:23:34 +0100

    Adapt to API changes

--- src/metrics/server/server.ts
+++ src/metrics/server/server.ts
@@ -1,26 +1,26 @@
 import {
     InitializeResult,
-    IPCMessageReader,
-    IPCMessageWriter,
-    IConnection,
     createConnection,
     TextDocuments,
-} from "vscode-languageserver";
+    TextDocumentSyncKind,
+    ProposedFeatures,
+} from "vscode-languageserver/node";
+import { TextDocument } from "vscode-languageserver-textdocument";
 import { MetricsRequestType } from "../common/protocol";
 import { MetricsParserUtil } from "./MetricsParserUtil";
 
-let connection: IConnection = createConnection(new IPCMessageReader(process), new IPCMessageWriter(process));
+let connection = createConnection(ProposedFeatures.all);
 
 console.log = connection.console.log.bind(connection.console);
 console.error = connection.console.error.bind(connection.console);
 
-let documents: TextDocuments = new TextDocuments();
+let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
 documents.listen(connection);
 
 connection.onInitialize((): InitializeResult => {
     return {
         capabilities: {
-            textDocumentSync: documents.syncKind,
+            textDocumentSync: TextDocumentSyncKind.Incremental,
         },
     };
 });
